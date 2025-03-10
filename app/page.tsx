51710d99c4dcdcc49875933b62bd268cce70cffe
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-03-10 05:36:51 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen w-screen'>
+      <div clas
+      >
+
+      </div>
+    </div>
+  )
+}
+
+export default page
